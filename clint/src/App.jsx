92779e39d8c1c92e7ae59805f734c45aa357dbbd
Aryan Yadav <caryan830@gmail.com>
2026-05-23 01:43:24 +0530

    adding new frontend

---
+++ clint/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className='bg-red-600'>
+        <div>this siss the </div>
+         </div>
+    </>
+  )
+}
+
+export default App
